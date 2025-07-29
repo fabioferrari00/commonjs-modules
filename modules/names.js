4d569d1e3fd2e3fcfc4aeea6e1cddd10feae323a
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-07-29 13:55:00 +0200

    first commit

---
+++ modules/names.js
@@ -0,0 +1,10 @@
+//creo una funzione che accetta due parametri di cui mi restituirà un oggetto
+
+const names = (firstName, lastName) => {
+  return {
+    firstName,
+    lastName
+  }
+}
+
+module.exports = names;
